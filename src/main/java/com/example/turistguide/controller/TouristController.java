ebc56fc7e7e2f31c2347b3e3ccc1df5dba03203f
Simon BC <simon.bech.christensen.00@gmail.com>
2026-02-19 18:42:51 +0100

    oprettet add GetMapping, og begyndt på html addnewattraction.

--- src/main/java/com/example/turistguide/controller/TouristController.java
+++ src/main/java/com/example/turistguide/controller/TouristController.java
@@ -1,11 +1,12 @@
 package com.example.turistguide.controller;
 
 import com.example.turistguide.model.TouristAttraction;
-import com.example.turistguide.model.UpdateRequest;
+import com.example.turistguide.repository.TouristRepository;
 import com.example.turistguide.service.TouristService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -14,10 +15,10 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/attractions")
-public class
-TouristController {
+public class TouristController {
 
     private final TouristService service;
+    private TouristRepository repository;
 
     public TouristController(TouristService touristService) {
         this.service = touristService;
@@ -44,34 +45,31 @@ TouristController {
         return "attraction";
     }
 
-    @PostMapping("/add")
-    public String addAttraction(@ModelAttribute TouristAttraction touristAttraction) {
-
-        for (TouristAttraction a : service.getAllAttractions()) {
-
-            if (touristAttraction.getName().equalsIgnoreCase(a.getName())) {
-                throw new ResponseStatusException(HttpStatus.CONFLICT, "Attraction already exists");
-            }
-        }
-        service.createAttraction(touristAttraction);
-        return "redirect:/attractions/save";
+    @GetMapping("/add")
+    public String addAttraction(Model model) {
+        TouristAttraction attraction = new TouristAttraction();
+        model.addAttribute("attraction", attraction);
+        model.addAttribute("cities", repository.getCities());
+        model.addAttribute("tags", repository.getTags());
+        return "addnewattraction";
     }
 
-    @PostMapping("/update")
-    public ResponseEntity<TouristAttraction> updateAttraction(@RequestBody UpdateRequest request) {
 
-        TouristAttraction foundAttraction = null;
-
-        for (TouristAttraction attraction : service.getAllAttractions()) {
-
-            if (request.getOldName().equals(attraction.getName())) {
-                foundAttraction = attraction;
-                service.updateAttraction(foundAttraction, request.getNewName(), request.getNewDescription());
-                return ResponseEntity.ok(foundAttraction);
-            }
-        }
-        return ResponseEntity.notFound().build();
-    }
+//    @PostMapping("/update")
+//    public ResponseEntity<TouristAttraction> updateAttraction(@RequestBody UpdateRequest request) {
+//
+//        TouristAttraction foundAttraction = null;
+//
+//        for (TouristAttraction attraction : service.getAllAttractions()) {
+//
+//            if (request.getOldName().equals(attraction.getName())) {
+//                foundAttraction = attraction;
+//                service.updateAttraction(foundAttraction, request.getNewName(), request.getNewDescription());
+//                return ResponseEntity.ok(foundAttraction);
+//            }
+//        }
+//        return ResponseEntity.notFound().build();
+//    }
 
 
     @PostMapping("/delete/{name}")
@@ -87,6 +85,5 @@ TouristController {
         }
         return ResponseEntity.notFound().build();
     }
-
 
 }
--- src/main/java/com/example/turistguide/service/TouristService.java
+++ src/main/java/com/example/turistguide/service/TouristService.java
@@ -44,4 +44,5 @@ public class TouristService {
 
         repository.deleteAttraction(attraction);
     }
+
 }
