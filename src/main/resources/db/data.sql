4a37882d33509cf1cbf3637f4e3c7c5fcd00ddd5
Mowgli-dk <emiland4@gmail.com>
2026-03-17 13:14:44 +0100

    description update in data.sql

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -15,20 +15,20 @@ INSERT IGNORE INTO tags (tag_name) VALUES
 
 INSERT INTO attractions (name, description, city_id)
 VALUES
-    ('Eiffel Tower', 'An impressive tower in the heart of Paris...',
+    ('Eiffel Tower', 'An impressive tower in the heart of Paris, with a great view. Close to many other attractions.',
      (SELECT city_id FROM cities WHERE city_name = 'Paris')),
 
-    ('Great Wall of China', 'An impressive wall spanning the northern parts of China...',
+    ('Great Wall of China', 'An impressive wall spanning the northern parts of China. Spend hours walking the long paths, and taking in the impressive sights.',
      (SELECT city_id FROM cities WHERE city_name = 'Beijing')),
 
-    ('The Little Mermaid', 'A small statue of a mermaid...',
+    ('The Little Mermaid', 'A small statue of a mermaid, calling back to the great danish writer H.C.Andersen. Its since, almost, become a symbol of the city of Copenhagen itself.',
      (SELECT city_id FROM cities WHERE city_name = 'Copenhagen')),
 
-    ('Grand Canyon', 'A historical site, of great value...',
+    ('Grand Canyon', 'A historical site, of great value to both archeologists, geologists and tourists who flock to it for the great view.',
      (SELECT city_id FROM cities WHERE city_name = 'Arizona')),
 
-    ('Tower Of London', 'An old fort...',
+    ('Tower Of London', 'An old fort, having had funtioned as a tollhouse, a prison and a seat of governance. An important building to the city of London.',
      (SELECT city_id FROM cities WHERE city_name = 'London')),
 
-    ('The Louvre', 'A world famous museum...',
+    ('The Louvre', 'A world famous museum and art gallery housing some of the most famous artworks in the world.',
      (SELECT city_id FROM cities WHERE city_name = 'Paris'));
