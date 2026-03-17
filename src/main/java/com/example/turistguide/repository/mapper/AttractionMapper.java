8515226fabee56639206854626f25b4b64890c35
R4X05Z3R0 <razeroeuw@gmail.com>
2026-03-18 00:34:25 +0100

    Fixed mapping issue in AttractionMapper

--- src/main/java/com/example/turistguide/repository/mapper/AttractionMapper.java
+++ src/main/java/com/example/turistguide/repository/mapper/AttractionMapper.java
@@ -6,18 +6,22 @@ import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class AttractionMapper implements RowMapper<TouristAttraction> {
 
     @Override
     public TouristAttraction mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Set<TouristTags> tagsSet = new HashSet<>();
+        tagsSet.add(TouristTags.valueOf(rs.getString("tags")));
 
         return new TouristAttraction(
                 rs.getLong("id"),
                 rs.getString("name"),
                 rs.getString("description"),
                 rs.getString("city"),
-                rs.getString("tags")
+                tagsSet
         );
     }
 }
