8d6eb3885da739f1649c16d0a05cf4e67584dfd1
Mowgli-dk <emiland4@gmail.com>
2026-03-17 13:10:36 +0100

    updated data.sql file

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,34 +1,34 @@
 
+INSERT IGNORE INTO cities (city_name) VALUES
+('Paris'),
+('Beijing'),
+('Copenhagen'),
+('Arizona'),
+('London');
 
-INSERT INTO cities (city_name) VALUES
-('Paris'), ('Beijing', ('Copenhagen'), ('Arizona'), ('London'))
-ON CONFLICT (city_name) DO NOTHING;
 
-
-INSERT INTO tags (tag_name) VALUES
+INSERT IGNORE INTO tags (tag_name) VALUES
 ('BØRNEVENLIG'), ('GRATIS'), ('MUSEUM'), ('DYR'),
 ('KUNST'), ('NATUR'), ('SIGHTSEEING'), ('OPLEVELSE'),
-('MINDESMÆRKE'), ('VERDENSKENDT')
-ON CONFLICT (tag_name) DO NOTHING;
+('MINDESMÆRKE'), ('VERDENSKENDT');
+
 
 INSERT INTO attractions (name, description, city_id)
 VALUES
-    ('Eiffel Tower', 'An impressive tower in the heart of Paris, with a great view. Close to many other attractions.',
-    (SELECT city_id FROM cities WHERE city_name = 'Paris')),
+    ('Eiffel Tower', 'An impressive tower in the heart of Paris...',
+     (SELECT city_id FROM cities WHERE city_name = 'Paris')),
 
-    ('Great Wall of China', 'An impressive wall spanning the northern parts of China. Spend hours walking the long paths, and taking in the impressive sights.',
+    ('Great Wall of China', 'An impressive wall spanning the northern parts of China...',
      (SELECT city_id FROM cities WHERE city_name = 'Beijing')),
 
-    ('The Little Mermaid', 'A small statue of a mermaid, calling back to the great danish writer H.C.Andersen. Its since, almost, become a symbol of the city of Copenhagen itself.',
+    ('The Little Mermaid', 'A small statue of a mermaid...',
      (SELECT city_id FROM cities WHERE city_name = 'Copenhagen')),
 
-    ('Grand Canyon', 'A historical site, of great value to both archeologists, geologists and tourists who flock to it for the great view.',
+    ('Grand Canyon', 'A historical site, of great value...',
      (SELECT city_id FROM cities WHERE city_name = 'Arizona')),
 
-    ('Tower Of London', 'An old fort, having had funtioned as a tollhouse, a prison and a seat of governance. An important building to the city of London.',
+    ('Tower Of London', 'An old fort...',
      (SELECT city_id FROM cities WHERE city_name = 'London')),
 
-    ('The Louvre', 'A world famous museum and art gallery housing some of the most famous artworks in the world.',
-     (SELECT city_id FROM cities WHERE city_name = 'Paris'))
-
-
+    ('The Louvre', 'A world famous museum...',
+     (SELECT city_id FROM cities WHERE city_name = 'Paris'));
